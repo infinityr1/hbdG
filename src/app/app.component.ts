3aa4340e4f0e8ec6c51ec03e73faee647300f7f8
unknown <nayak.ranjeetkumar@gmail.com>
2021-01-13 01:08:41 +0530

    11-7th Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,34 +50,5 @@ export class AppComponent implements OnInit {
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
-
-
-
-
-
-
-
-
-  // playAudio(){
-  //   let audio = new Audio();
-  //   let audio1 = new Audio();
-  //   let audio2 = new Audio();
-  //   //audio.src = "../assets/v.mp3";
-  //   //audio1.src = "../assets/v1.mp3";
-  //   audio2.src = "./assets/v2.mp3";
-  //   // audio.load();
-  //   // audio.play();
-  //   // audio1.load();
-  //   // audio1.play();
-  //   let i = 0;
-  //   for (i =0;i <5;i++)
-  //   {
-  //     audio2.load();
-  //     audio2.play();
-  //     i++;
-  //   }
-    
-  //   }
-
  
 }
